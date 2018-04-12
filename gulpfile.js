bcbdd240829c18afb5ff8f014c5b4a2bcf32c110
zhangliangliang <zhangliangliang@yunzongnet.com>
2018-04-12 13:48:11 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,11 @@ var clean =	require('gulp-clean');
  //    var streamify = require('gulp-streamify');//解决插件不支持 stream 的问题
 var sass = require('gulp-sass');
 var bs=require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var  rev = require('gulp-rev');
 var reload = bs.reload;
 var path=require('path');
+var rename = require('gulp-rename');
 gulp.task('clean',function(){
 	return gulp.src('./dist')
 		.pipe(clean());
@@ -25,13 +28,15 @@ gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     // .pipe(gulp.dest('./dist/css'))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./src/css'))
     .pipe(reload({stream: true}));
 });
 // js
-gulp.task('js:build', function () {
+gulp.task('js', function () {
   return gulp.src('./src/js/**/*.js')
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./src/js'))
     .pipe(reload({stream: true}));
 });
 
@@ -39,7 +44,7 @@ gulp.task('js:build', function () {
 //   gulp.watch('./src/sass/**/*.scss', ['sass']);
 // });
 //压缩js;
-gulp.task('compress', function (cb) {
+gulp.task('compress',function (cb) {
       pump([
             gulp.src('src/**/*.js'),
             uglify(),
@@ -65,13 +70,31 @@ gulp.task('dev:server',['sass'],function(){
           open: "local",
       });
       gulp.watch("./src/**/*.scss", ['sass']);
-      // gulp.watch("src/js/**/*.js", ['js']);
+      gulp.watch("src/js/**/*.js", ['js']);
       gulp.watch("./src/*.html").on('change', reload);
 })
 
-gulp.task('build',['html','sass','js:build'],function(){
+gulp.task('build',['html','sass'],function(){
 
 });
+// 图片压缩
+gulp.task('minimg', () =>
+  gulp.src('src/images/*')
+    .pipe(imagemin([
+          imagemin.jpegtran({progressive: true}),
+          imagemin.optipng({optimizationLevel: 5}),
+    ]))
+    .pipe(gulp.dest('dist/images'))
+);
+//静态文件添加指纹;
+gulp.task('hash',() =>
+    gulp.src(['./src/css/*.css', './src/js/*.js'], {base: 'src'})
+        .pipe(gulp.dest('dist'))  // copy original assets to build dir
+        .pipe(rev())
+        .pipe(gulp.dest('dist'))  // write rev'd assets to build dir
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('src'))  // write manifest to build dir
+);
 // 开始一个Browsersync代理
 // bs.init({
 //     proxy: "http://www.bbc.co.uk"
